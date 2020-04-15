db148c437d46cb60f0344a875478b605af8e1d42
Stephen Liu <ssl76@case.edu>
2020-04-15 16:33:20 -0500

    Compute the second order terms using my simplifications for the doubly indexed sums.

--- src/secondorder.m
+++ src/secondorder.m
@@ -2,9 +2,11 @@ ds = [1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41];
 seconds = zeros(length(ds),1);
 secondsmu = zeros(length(ds),1);
 secondsimp = zeros(length(ds),1);
+secondsimp2 = zeros(length(ds),1);
 firsts = zeros(length(ds),1);
 quos = zeros(length(ds),1);
 quos2 = zeros(length(ds),1);
+quos3 = zeros(length(ds),1);
 
 for i = 1:length(ds)
     d = ds(i);
@@ -23,19 +25,54 @@ for i = 1:length(ds)
     quos(i) = (secondsmu(i)/seconds(i));
     quos2(i) = (secondsimp(i)/secondsmu(i));
     firsts(i) = 0.5*V1(m);
+    
+    % Try out the simplifications I got for the doubly indexed sums
+    % First sum:
+    firstsum = 0;
+    for p2 = 2:(m+1)
+        for q1 = 0:(p2-2)
+            for q2 = 0:(m+1-p2)
+                firstsum = firstsum + kratio(2,q1+1)*(1/(2*p2-1))*kratio(p2+1,p2+q2);
+            end
+        end
+    end
+    
+    % Second sum:
+    secondsum = 2*m^2/((2*m-1)*(2*m+1))-(2*m*sqrt(pi)*gamma(m))/((2*m-1)*(2*m+1)*gamma(m-0.5));
+    
+    % Third sum:
+    thirdsum = (m+1)/(2*m+1)-(sqrt(pi)*gamma(m+1))/((2*m+1)*gamma(m+0.5));
+    
+    % Fourth sum:
+    fourthsum = 0;
+    for p1 = 2:m
+        for p2 = (p1+1):(m-1)
+            for q1 = 0:(p2-p1-1)
+                fourthsum = fourthsum + (1/(2*p1-1))*kratio(p1+1,p1+q1)*(1/(2*p2-1))*kratio(p2+1,m+1);
+            end
+        end
+    end
+
+    %Dsum:
+    dsum = V1(m)*((m+1)-(2^(2*m)*(factorial(m))^2)/(factorial(2*m)));
+    
+    secondsimp2(i) = 6*(firstsum + secondsum + thirdsum + fourthsum) - dsum;
+    
+    quos3(i) = secondsimp2(i)/secondsimp(i);
 end
 
 figure(1);
-% plot(ds, firsts, 'r--o');
-% hold on;
-% plot(ds, seconds, 'b--o');
-% plot(ds, secondsmu, 'g--o');
-% plot(ds, secondsimp, 'k--o');
-% hold off;
-% legend('Firsts', 'Seconds', 'Secondsmu', 'Secondsimp', 'Location', 'NorthWest');
+plot(ds, firsts, 'r--o');
+hold on;
+plot(ds, seconds, 'b--o');
+%plot(ds, secondsmu, 'g--o');
+plot(ds, secondsimp, 'k--o');
+plot(ds, secondsimp2, 'g--o');
+hold off;
+legend('Firsts', 'Seconds', 'Secondsimp', 'Secondsimp2', 'Location', 'NorthWest');
 
-plot(ds, secondsimp, 'b--o');
-xlabel('Dimension d');
-ylabel('Second order term');
-title('Second order term vs. Dimension');
-set(gca, 'FontSize', 15);
+% plot(ds, secondsimp, 'b--o');
+% xlabel('Dimension d');
+% ylabel('Second order term');
+% title('Second order term vs. Dimension');
+% set(gca, 'FontSize', 15);
